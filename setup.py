5d3f6aa1d5ca45d0617720f7ec51f9aa85c8ba9a
Pedro Torcatt <36505934+Jalkhov@users.noreply.github.com>
2021-09-18 08:50:20 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='testing-pypi-autorelease',
+    version='0.0.1',
+    description='For testing',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Pedro Torcatt',
+    author_email='pedrotorcattsoto@gmail.com',
+    url='https://github.com/dvl/python-internet-sabotage',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+    ],
+    keywords='test pypi',
+    packages=find_packages(),
+)
