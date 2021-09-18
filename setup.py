bf14eee8591d73ff1eabf95b699a800011525eee
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2021-09-18 10:16:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='testing-pypi-autorelease',
-    version='0.0.6',
+    version='0.0.7',
     description='For testing',
     long_description=long_description,
     long_description_content_type='text/markdown',
